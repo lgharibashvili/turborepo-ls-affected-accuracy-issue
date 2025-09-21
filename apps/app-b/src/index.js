9914ee0181a69cdfb4e898c5d5ed388c05b96548
0xb4lamx <medghaith.sellami@gmail.com>
2025-09-21 11:47:09 +0200

    add lodash to app-b

--- apps/app-b/src/index.js
+++ apps/app-b/src/index.js
@@ -1,3 +1,5 @@
 const { getPackageBMessage } = require('pkg-b');
+const _ = require('lodash');
 
 console.log('App B:', getPackageBMessage());
+console.log('App B with lodash:', _.upperCase('hello world'));
